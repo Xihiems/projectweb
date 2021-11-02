283219b4eff128b5b47d00ee7042e5afb5b1cf64
Yann Raymond <raymondyann581@gmail.com>
2021-11-02 22:50:38 +0100

    initialisation de la page header

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+
+  <c-flex  border-radius="10px" border-width="1px" w="100%" bg="gray" p="20px" flexWrap="no" color="white" justify-content="space-between"  >
+    <c-box> Nomdusite</c-box>
+    <c-box> Nos produits</c-box>
+    <c-box> Login</c-box>
+
+  </c-flex>
+
+</template>
+
+
+<script>
+import { CBox,CFlex } from '@chakra-ui/vue';
+
+export default {
+  name: 'Header',
+  components: {
+    CBox,
+    CFlex,
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
