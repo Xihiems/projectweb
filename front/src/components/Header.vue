49bc39d68ee71c03c6aa64c677c3b063e18ad3f5
Yann Raymond <raymondyann581@gmail.com>
2021-11-04 21:22:35 +0100

    initialisation de la page catalog + ajout du router à la page principal

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -1,21 +1,22 @@
 <template>
 
   <c-flex  border-radius="10px" border-width="1px" w="100%" bg="gray" p="0.75em" flexWrap="no" color="white" justify-content="space-between"  >
-    <c-box as="button" > CompSell 🖥️</c-box>
-    <c-box as="button"> Nos Produits</c-box>
+    <c-box as="button" ><c-link as="router-link" to="/"> CompSell 🖥️</c-link></c-box>
+    <c-box as="button"><c-link as="router-link" to="/catalog">Nos Produits</c-link></c-box>
     <c-box as="button" > Login </c-box>
   </c-flex>
 
 </template>
 
 <script>
-import { CBox, CFlex } from '@chakra-ui/vue'
+import { CBox, CFlex, CLink } from '@chakra-ui/vue'
 
 export default {
   name: 'Header',
   components: {
     CBox,
-    CFlex
+    CFlex,
+    CLink
   }
 }
 </script>
---
+++ front/src/views/Catalog.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div>
+      <Header></Header>
+    </div>
+    <div><c-heading mt="1em">All our product</c-heading></div>
+    <c-flex ml="10em" mt="1em" w="12.5em">
+      <c-select v-model="Sorttype" placeholder="Default">
+        <option value="alphabétique"> Alphabetical</option>
+        <option value="Priceascending"> Ascending price</option>
+        <option value="Pricedecreasing"> Decreasing price</option>
+      </c-select>
+    </c-flex>
+    <c-flex>
+      <Footer/>
+    </c-flex>
+  </div>
+</template>
+
+<script>
+
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+import { CHeading, CFlex, CSelect } from '@chakra-ui/vue'
+export default {
+  name: 'HomePage',
+  components: {
+    Header,
+    CHeading,
+    CFlex,
+    CSelect,
+    Footer
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
